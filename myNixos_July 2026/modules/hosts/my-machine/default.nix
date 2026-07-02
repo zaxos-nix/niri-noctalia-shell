908f3a04192602a4e4d3595ed50e93f291cd20e3
Isaac "Izo" Njoroge <selfmgnt@gmail.com>
2026-07-02 20:20:20 +0300

    Delete myNixos_July 2026/modules directory

--- myNixos_July 2026/modules/hosts/my-machine/default.nix
+++
@@ -1,8 +0,0 @@
-{ self, inputs, ... }: {
-  flake.nixosConfigurations.myMachine = inputs.nixpkgs.lib.nixosSystem {
-    modules = [
-      self.nixosModules.myMachineConfiguration
-      
-    ];
-  };
-}
